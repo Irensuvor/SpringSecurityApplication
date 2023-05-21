3e5e45f6ae49598da41db4500f09cf53793905e6
Everaiden <martynenko3131@gmail.com>
2023-05-22 03:39:45 +0700

    update 1 - add_4_5_mark_tasks_1
    add order list and user list (nw) for admin page (nw), add service and controller for orders, update order repository

--- src/main/java/com/example/springsecurityapplication/repositories/OrderRepository.java
+++ src/main/java/com/example/springsecurityapplication/repositories/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.springsecurityapplication.repositories;
 import com.example.springsecurityapplication.models.Order;
 import com.example.springsecurityapplication.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByPerson(Person person);
+    @Query(value = "select * from product where (RIGHT(number, 4) LIKE %?1)",nativeQuery = true)
+    List<Order> findByLastFourOfNumber(String number);
 }
